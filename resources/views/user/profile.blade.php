19e973b0c7f2a23eef9b2a13b70295d9248a1b0f
ArtursAsi <WARlock6665>
2020-06-19 11:24:56 +0300

    Added modal to profile picture

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -38,16 +38,11 @@
                 <div class="card" style="width: 18rem;">
                     <div class="card-header"><p>Profile</p></div>
 
-                    <a href="{{$user->userData->getPicture()}}"><img src="{{$user->userData->getPicture()}}" alt="" style="width: 100%"></a>
+                    <a href="" data-toggle="modal" data-target="#myModal"><img src="{{$user->userData->getPicture()}}" alt=""
+                                                                     style="width: 100%"></a>
 
                     <div class="card-body">
-                        <form action="{{route('user.updatePicture',$user)}}" enctype="multipart/form-data"
-                              method="post">
-                            @csrf
-                            @method('PATCH')
-                            <input type="file" name="picture">
-                            <button class="btn btn-outline-primary">Upload</button>
-                        </form>
+
                         <p class="card-text">Name: {{$user->userData->name}}</p>
                         <p class="card-text">Surname: {{$user->userData->surname}}</p>
                         <p class="card-text">Age: {{$user->userData->age}}</p>
@@ -60,6 +55,32 @@
                         <a href="{{route('user.password',$user)}}">Change Password</a>
                         <a href="{{route('user.email',$user)}}">Change E-mail</a>
 
+                    </div>
+                </div>
+            </div>
+            <div class="modal" tabindex="-1" role="dialog" id="myModal">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">My Picture</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="{{$user->userData->getPicture()}}" alt=""
+                                 style="width: 100%">
+                        </div>
+                        <div class="modal-footer">
+                            <form action="{{route('user.updatePicture',$user)}}" enctype="multipart/form-data"
+                                  method="post">
+                                @csrf
+                                @method('PATCH')
+                                <input type="file" name="picture">
+                                <button class="btn btn-outline-primary">Upload</button>
+                            </form>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -87,6 +108,26 @@
                         <a href="{{route('user.show',$user)}}">
                             <img src="{{$user->userData->getPicture()}}" alt="" style="width: 100%">
                         </a>
+                        <div class="modal" tabindex="-1" role="dialog" id="exampleModal">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Modal title</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img src="{{$user->userData->getPicture()}}" alt=""
+                                             style="width: 100%">
+                                    </div>
+                                    <div class="modal-footer">
+
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div class="card-body">
                             <p class="card-text">Name: {{$user->userData->name}}</p>
                             <p class="card-text">Surname: {{$user->userData->surname}}</p>
@@ -107,6 +148,12 @@
     </div>
 
 
+
+    <script>
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#myInput').trigger('focus')
+        })
+    </script>
 
 
 
